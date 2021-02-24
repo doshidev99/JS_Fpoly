ff4e0cda66f4ff5b9edfd4475e8565099b85f0be
phamdongtruong99 <truongpd.doublenine@gmail.com>
2021-02-24 19:41:34 +0700

    crud-admin

--- JS_advanced/Assignment/app/admin/js/index.js
+++ JS_advanced/Assignment/app/admin/js/index.js
@@ -1,4 +1,4 @@
-// const el_category = document.getElementById("category");
+const urlProduct = "http://localhost:3000/products";
 
 const requestApi = (url, method = "GET", data) => {
   const response = fetch(url, {
@@ -41,41 +41,36 @@ const lengthAllProduct = document.getElementById("length-all-product");
 requestApi("http://localhost:3000/products")
   .then((response) => response.json()) // convert to json
   .then(async (result) => {
-    result.forEach((el, index) => {
-      renderProducts(el, index);
-    });
-
     lengthAllProduct.innerText = result?.length;
-
-    // const btnDel = document.querySelector(`.btn-del`);
-
-    // btnDel.addEventListener("click", (e) => {
-    //   console.log("clicked");
-    // });
+    const html = result
+      .map((el, index) => {
+        return `
+          <tr>
+            <td>${index + 1}</td>
+            <td>${el.name}</td>
+            <td> <img style="width: 40px" src="./assets/img/product/${
+              el.image
+            }" alt="" /> </td>
+            <td>${el.price}</td>
+            <td>${el.detail}</td>
+            <td>${el.discount * 100}%</td>
+            <td>${el.status === "1" ? "còn hàng" : "hết hàng"}</td>
+            <td>
+              <button class="btn-edit btn btn-success"
+              data-bs-toggle="modal" data-bs-target="#exampleModal"
+              onClick="handleEdit(${el.id})"> Edit </button>
+              <button class="btn-del btn btn-danger"  onClick="handleDelete(${
+                el.id
+              })"> Delete </button>
+            </td>
+          </tr>
+        `;
+      })
+      .join(" ");
+    listProduct.innerHTML = html;
   })
   .catch((err) => console.log("Request Failed", err));
 
-const renderProducts = (el, index) => {
-  const ouput = `
-    <tr>
-      <td>${index + 1}</td>
-      <td>${el.name}</td>
-      <td> <img style="width: 40px" src="./assets/img/product/${
-        el.image
-      }" alt="" /> </td>
-      <td>${el.price}</td>
-      <td>${el.detail}</td>
-      <td>${el.discount * 100}%</td>
-      <td>${el.status === 1 ? "còn hàng" : "hết hàng"}</td>
-      <td>
-        <div id="" class="btn-edit btn btn-success"> Edit </div>
-        <div id="btn-del" class="btn-del btn btn-danger"> Delete </div>
-      </td>
-    </tr>
-  `;
-
-  listProduct.insertAdjacentHTML("beforeend", ouput);
-};
 // search category
 const searchCategory = document.getElementById("search-category");
 
@@ -103,10 +98,14 @@ searchCategory.addEventListener("submit", (e) => {
             <td>${el.price}</td>
             <td>${el.detail}</td>
             <td>${el.discount * 100}%</td>
-            <td>${el.status === 1 ? "còn hàng" : "hết hàng"}</td>
+            <td>${el.status === "1" ? "còn hàng" : "hết hàng"}</td>
             <td>
-              <div id="" class="btn-edit btn btn-success"> Edit </div>
-              <div id="btn-del" class="btn-del btn btn-danger"> Delete </div>
+            <button class="btn-edit btn btn-success"
+              data-bs-toggle="modal" data-bs-target="#exampleModal"
+              onClick="handleEdit(${el.id})"> Edit </button>
+            <button class="btn-del btn btn-danger"  onClick="handleDelete(${
+              el.id
+            })"> Delete </button>
             </td>
           </tr>
       `;
@@ -116,3 +115,54 @@ searchCategory.addEventListener("submit", (e) => {
     })
     .catch((err) => console.log("Request Failed", err));
 });
+
+const handleDelete = (id) => {
+  requestApi(`http://localhost:3000/products/${id}`, "DELETE");
+};
+
+// edit modal
+
+const editProduct = document.querySelector(".form-product-edit");
+
+editProduct.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const {
+    name,
+    price,
+    detail,
+    image,
+    discount,
+    status,
+    category,
+    id,
+    imgTemp,
+  } = editProduct;
+
+  requestApi(`${urlProduct}/${id.value}`, "PATCH", {
+    id: id.value,
+    name: name.value,
+    cate_id: "Herc Dangl",
+    price: price.value,
+    detail: detail.value,
+    image: image?.files[0]?.name ? image.files[0].name : imgTemp.value,
+    discount: discount.value / 100,
+    status: status.value,
+    category: category.value,
+  });
+});
+
+const handleEdit = (id) => {
+  requestApi(`http://localhost:3000/products/${id}`)
+    .then((response) => response.json())
+    .then(async (result) => {
+      editProduct.id.value = result.id;
+      editProduct.name.value = result.name;
+      editProduct.price.value = Number(result.price);
+      editProduct.discount.value = result.discount * 100;
+      editProduct.detail.value = result.detail;
+      editProduct.status.value = result.status;
+      editProduct.imgTemp.value = result.image;
+    })
+    .catch((err) => console.log("Request Failed", err));
+};
